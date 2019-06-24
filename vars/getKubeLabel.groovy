d861850ebea9afc9424238a58016973c05e8cc8a
enricovianello <maverick17586@gmail.com>
2019-06-24 16:37:20 +0200

    back to label assignment

--- vars/getKubeLabel.groovy
+++ vars/getKubeLabel.groovy
@@ -5,12 +5,15 @@ def call(script) {
   def String clearedJobName = JOB_NAME.split('/')[0]
   def String clearedJobBaseName = JOB_BASE_NAME.replaceAll('%2F','-')
   def String jobBuildNumber = BUILD_NUMBER
+  def String label = ""
 
   if (JOB_NAME != JOB_BASE_NAME) {
       // multi branch pipeline
-      return clearedJobName + '-' + clearedJobBaseName + '-' + jobBuildNumber
+      label = clearedJobName + '-' + clearedJobBaseName + '-' + jobBuildNumber
   } else {
       // simple pipeline
-      return clearedJobName + '-' + jobBuildNumber
+      label = clearedJobName + '-' + jobBuildNumber
   }
+
+  return label
 }

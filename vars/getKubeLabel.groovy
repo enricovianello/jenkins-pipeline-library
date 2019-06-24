4edb893fff915f6dceadae7415f4092204882245
enricovianello <maverick17586@gmail.com>
2019-06-24 15:47:30 +0200

    Added getKubeLabel

---
+++ vars/getKubeLabel.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+def call(String jobName, String jobBaseName, String jobBuildNumber) {
+  def clearedJobName = jobName.split('/')[0]
+  def clearedJobBaseName = jobBaseName.replaceAll('%2F','-')
+  if jobName != jobBaseName {
+      // multi branch pipeline
+      return clearedJobName + '-' + clearedJobBaseName + '-' + jobBuildNumber
+  }
+  return clearedJobName + '-' + jobBuildNumber
+}

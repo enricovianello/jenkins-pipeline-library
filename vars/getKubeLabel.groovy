3a94abe104576b747f6aeba559befa0a6510c442
enricovianello <maverick17586@gmail.com>
2019-06-24 15:52:53 +0200

    Fixed syntax error

--- vars/getKubeLabel.groovy
+++ vars/getKubeLabel.groovy
@@ -3,7 +3,7 @@
 def call(String jobName, String jobBaseName, String jobBuildNumber) {
   def clearedJobName = jobName.split('/')[0]
   def clearedJobBaseName = jobBaseName.replaceAll('%2F','-')
-  if jobName != jobBaseName {
+  if (jobName != jobBaseName) {
       // multi branch pipeline
       return clearedJobName + '-' + clearedJobBaseName + '-' + jobBuildNumber
   }

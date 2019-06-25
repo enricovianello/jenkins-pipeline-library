d13c3a91ef2cac4934e5ae34f1d2931dafbb3e01
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2019-06-25 16:00:20 +0200

    add build- prefix to kube label

--- vars/getKubeLabel.groovy
+++ vars/getKubeLabel.groovy
@@ -5,7 +5,7 @@ def call(Closure script) {
   def String clearedJobName = JOB_NAME.split('/')[0]
   def String clearedJobBaseName = JOB_BASE_NAME.replaceAll('%2F','-')
   def String jobBuildNumber = BUILD_NUMBER
-  def String label = ""
+  def String label = "build-"
 
   if (JOB_NAME != JOB_BASE_NAME) {
       // multi branch pipeline
